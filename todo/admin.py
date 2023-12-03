45c127883f03c42d2656b9cc2d673a019ad1462c
Camdah77 <>
2023-12-03 15:59:10 +0000

    models added in admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+admin.site.register(Item)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Item(models.Model):
+
+    name = models.CharField(max_length=100, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
+    
