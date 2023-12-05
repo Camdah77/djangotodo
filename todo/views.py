d3668d19f7cf666d3062bae1f7fad8c714163211
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2023-12-05 15:07:27 +0000

    test files created

--- todo/views.py
+++ todo/views.py
@@ -2,13 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
 
-def get_todo_list(request):
-    items = Item.objects.all()
-    context = {
-        'items': items
-    }
-    return render(request, 'todo/todo_list.html', context)
 
+# Create your views here.
 
 def get_todo_list(request):
     items = Item.objects.all()
@@ -47,3 +42,14 @@ def edit_item(request, item_id):
     return render(request, "todo/edit_item.html", context)
 
 
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect("get_todo_list")
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect("get_todo_list")
